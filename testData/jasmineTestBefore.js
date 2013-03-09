421475a9b4a3fb2156ba22fae066f835a971aa4f
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-03-09 14:18:22 -0500

    Add test date

---
+++ testData/jasmineTestBefore.js
@@ -0,0 +1,24 @@
+describe('top describe', function () {
+    it('first it', function () {
+        console.log('WWWW');
+    });
+
+    it('second it', function () {
+        console.log('WWWW');
+    });
+
+    describe('inner describe', function () {
+        it('inner it 1', function () {
+            var a;
+        });
+
+        it('inner it 2', function () {
+            <caret>
+            var a;
+        });
+
+        iit('inner it 3', function () {
+            var a;
+        });
+    });
+});
