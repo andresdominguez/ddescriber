5dd106c3849035b36652b716253e2ada992dd744
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-03-09 14:41:48 -0500

    First unit test passing

---
+++ test/com/karateca/ddescriber/JasmineFinderTest.java
@@ -0,0 +1,41 @@
+package com.karateca.ddescriber;
+
+import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.editor.impl.DocumentImpl;
+import com.intellij.openapi.editor.impl.EditorImpl;
+import com.intellij.psi.PsiFile;
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+import junit.framework.Assert;
+
+import java.io.File;
+
+/**
+ * @author Andres Dominguez.
+ */
+public class JasmineFinderTest extends LightCodeInsightFixtureTestCase {
+
+  private JasmineFinder jasmineFinder;
+
+  @Override
+  protected String getTestDataPath() {
+    String testPath = PathManager.getJarPathForClass(JasmineFinderTest.class);
+    File sourceRoot = new File(testPath, "../../..");
+    return new File(sourceRoot, "testData").getPath();
+  }
+
+  public void setUp() throws Exception {
+    super.setUp();
+    PsiFile psiFile = myFixture.configureByFile("jasmineTestBefore.js");
+    jasmineFinder = new JasmineFinder(getProject(), new DocumentImpl(psiFile.getText()), psiFile.getVirtualFile());
+  }
+
+  public void testFindUnitTestsAndSuites() throws Exception {
+    // Given that you have a jasmine test file.
+
+    // When you find all the matches.
+    jasmineFinder.findAll();
+
+    // Then ensure the unit tests and the suites were found.
+    Assert.assertEquals(7, jasmineFinder.testFindResults.size());
+  }
+}
