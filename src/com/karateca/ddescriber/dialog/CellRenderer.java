5f95264c31d8655029f881f30016050a5a600945
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-03-10 15:38:15 -0400

    Paint background as green

--- src/com/karateca/ddescriber/dialog/CellRenderer.java
+++ src/com/karateca/ddescriber/dialog/CellRenderer.java
@@ -9,13 +9,16 @@ import java.awt.*;
  * @author Andres Dominguez.
  */
 class CellRenderer extends DefaultListCellRenderer {
+
+  public static final Color GREEN_BG_COLOR = new Color(182, 232, 172);
+
   @Override
   public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
     Component component = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
 
     // Paint the cell as green if it is an iit or ddescribe.
-    if (((TestFindResult) value).isMarkedForRun()) {
-      component.setForeground(new Color(0, 102, 0));
+    if (((TestFindResult) value).isMarkedForRun() && !isSelected) {
+      component.setBackground(GREEN_BG_COLOR);
     }
 
     return component;
