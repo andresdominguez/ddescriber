d1604c7a9ad676f85132066f5237ea28180106af
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-03-09 21:30:53 -0500

    Show a scroll pane and position the selected row in the middle

--- src/com/karateca/ddescriber/dialog/Dialog.java
+++ src/com/karateca/ddescriber/dialog/Dialog.java
@@ -3,11 +3,12 @@ package com.karateca.ddescriber.dialog;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.ui.components.JBList;
+import com.intellij.ui.components.JBScrollPane;
 import com.karateca.ddescriber.Hierarchy;
 import com.karateca.ddescriber.TestFindResult;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.JComponent;
+import javax.swing.*;
 import java.util.List;
 
 /**
@@ -16,6 +17,7 @@ import java.util.List;
 public class Dialog extends DialogWrapper {
   private final Hierarchy hierarchy;
   private JBList jbList;
+  public static final int VISIBLE_ROW_COUNT = 13;
 
   public Dialog(@Nullable Project project, Hierarchy hierarchy) {
     super(project);
@@ -27,18 +29,25 @@ public class Dialog extends DialogWrapper {
   @Nullable
   @Override
   protected JComponent createCenterPanel() {
-    createDialogList();
-    return jbList;
-  }
-
-  private void createDialogList() {
     List<TestFindResult> elements = hierarchy.getUnitTestsForCurrentDescribe();
+
     jbList = new JBList(elements.toArray());
+    jbList.setVisibleRowCount(VISIBLE_ROW_COUNT);
+
+    // Use a custom cell renderer to paint green.
     jbList.setCellRenderer(new CellRenderer());
 
     // Select the closest element found from the current position.
     int closestIndex = elements.indexOf(hierarchy.getClosest());
     jbList.setSelectedIndex(closestIndex);
+
+    // Create a scroll pane and make sure the selected row is in the middle.
+    JBScrollPane scrollPane = new JBScrollPane(jbList);
+    int minIndex = Math.max(0, closestIndex - VISIBLE_ROW_COUNT / 2);
+    int maxIndex = Math.min(elements.size() - 1, closestIndex + VISIBLE_ROW_COUNT / 2);
+    jbList.scrollRectToVisible(jbList.getCellBounds(minIndex, maxIndex));
+
+    return scrollPane;
   }
 
   @Nullable
