9ac59ada9b07fc3661336936cb82d3610cce52e8
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-03-11 23:48:29 -0400

    added createHierarchy

--- src/com/karateca/ddescriber/toolWindow/JasminFile.java
+++ src/com/karateca/ddescriber/toolWindow/JasminFile.java
@@ -1,6 +1,11 @@
 package com.karateca.ddescriber.toolWindow;
 
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.karateca.ddescriber.Hierarchy;
+import com.karateca.ddescriber.JasmineFinder;
 
 /**
  * @author Andres Dominguez.
@@ -8,13 +13,27 @@ import com.intellij.openapi.vfs.VirtualFile;
 public class JasminFile {
 
   private final VirtualFile virtualFile;
+  private Hierarchy hierarchy;
 
   public JasminFile(VirtualFile virtualFile) {
     this.virtualFile = virtualFile;
+  }
+
+  public Hierarchy createHierarchy(Project project) {
+    FileDocumentManager instance = FileDocumentManager.getInstance();
+    Document document = instance.getDocument(virtualFile);
+
+    JasmineFinder jasmineFinder = new JasmineFinder(project, document, virtualFile);
+    jasmineFinder.findAll();
+    hierarchy = new Hierarchy(document, jasmineFinder.getFindResults(), 0);
+
+    return hierarchy;
   }
 
   @Override
   public String toString() {
     return virtualFile.getName();
   }
+
+
 }
