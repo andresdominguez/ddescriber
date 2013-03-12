6f2bb8e9c3a584e6f5c3e34d61b6c6f25309fb75
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-03-11 23:18:56 -0400

    Hide the root node

--- src/com/karateca/ddescriber/toolWindow/JasmineToolwindow.java
+++ src/com/karateca/ddescriber/toolWindow/JasmineToolwindow.java
@@ -5,10 +5,15 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.SpeedSearchComparator;
+import com.intellij.ui.TreeSpeedSearch;
+import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import com.intellij.ui.treeStructure.Tree;
 
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
 import java.awt.*;
 import java.util.List;
 
@@ -40,9 +45,50 @@ public class JasmineToolWindow implements ToolWindowFactory {
   }
 
   private void showTestsInToolWindow(List<JasminFile> jasminFiles) {
+
     JPanel panel = new JPanel(new GridBagLayout());
+    GridBagConstraints gridBagConstraintsScrollPane = new GridBagConstraints();
+    gridBagConstraintsScrollPane.gridx = 0;
+    gridBagConstraintsScrollPane.gridy = 1;
+    gridBagConstraintsScrollPane.gridwidth = 10;
+    gridBagConstraintsScrollPane.gridheight = 10;
+    gridBagConstraintsScrollPane.fill = GridBagConstraints.BOTH;
+    gridBagConstraintsScrollPane.anchor = GridBagConstraints.CENTER;
+    gridBagConstraintsScrollPane.weightx = 1;
+    gridBagConstraintsScrollPane.weighty = 10;
+    panel.add(createCenterPanel(jasminFiles), gridBagConstraintsScrollPane);
+
+
     ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
     Content content = contentFactory.createContent(panel, "", false);
     toolWindow.getContentManager().addContent(content);
   }
+
+  private JComponent createCenterPanel(List<JasminFile> jasminFiles) {
+
+    DefaultMutableTreeNode root = new DefaultMutableTreeNode("All tests");
+
+    Tree tree = new Tree(root);
+
+    for (JasminFile jasminFile : jasminFiles) {
+      root.add(new DefaultMutableTreeNode(jasminFile.toString()));
+    }
+
+    //tree.setCellRenderer(new CustomTreeCellRenderer());
+
+    // Add search, make it case insensitive.
+    new TreeSpeedSearch(tree) {
+      @Override
+      protected boolean compare(String text, String pattern) {
+        return super.compare(text.toLowerCase(), pattern.toLowerCase());
+      }
+    }.setComparator(new SpeedSearchComparator(false));
+
+    JBScrollPane scrollPane = new JBScrollPane(tree);
+    tree.expandRow(0);
+    tree.setRootVisible(false);
+
+    return scrollPane;
+  }
+
 }
