4f9f6797b301a7556accae3823526389e92c5235
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-03-13 15:52:21 -0400

    Add beautiful icons

--- src/com/karateca/ddescriber/dialog/CustomTreeCellRenderer.java
+++ src/com/karateca/ddescriber/dialog/CustomTreeCellRenderer.java
@@ -1,5 +1,6 @@
 package com.karateca.ddescriber.dialog;
 
+import com.intellij.openapi.util.IconLoader;
 import com.karateca.ddescriber.TestFindResult;
 
 import javax.swing.*;
@@ -14,6 +15,8 @@ public class CustomTreeCellRenderer extends DefaultTreeCellRenderer {
 
   static final Color GREEN_BG_COLOR = new Color(182, 232, 172);
   private final Color defaultColor = getBackgroundNonSelectionColor();
+  private Icon itIcon = IconLoader.findIcon("/icons/it-icon.png");
+  private Icon descIcon = IconLoader.findIcon("/icons/desc-icon.png");
 
   @Override
   public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
@@ -24,6 +27,9 @@ public class CustomTreeCellRenderer extends DefaultTreeCellRenderer {
 
     Color color = findResult.isMarkedForRun() ? GREEN_BG_COLOR : defaultColor;
     setBackgroundNonSelectionColor(color);
+
+    // Set the icon depending on the type.
+    setIcon(findResult.isDescribe() ? descIcon : itIcon);
 
     return component;
   }
