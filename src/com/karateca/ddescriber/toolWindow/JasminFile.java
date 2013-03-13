64004e35e1f83933a97e4c8155d5b48c612a2c37
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-03-13 16:51:52 -0400

    Use action util to get doc

--- src/com/karateca/ddescriber/toolWindow/JasminFile.java
+++ src/com/karateca/ddescriber/toolWindow/JasminFile.java
@@ -1,31 +1,29 @@
 package com.karateca.ddescriber.toolWindow;
 
 import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.karateca.ddescriber.ActionUtil;
 import com.karateca.ddescriber.Hierarchy;
 import com.karateca.ddescriber.JasmineFinder;
 
 /**
  * @author Andres Dominguez.
  */
-public class JasminFile {
+class JasminFile {
 
   private final VirtualFile virtualFile;
-  private Hierarchy hierarchy;
 
   public JasminFile(VirtualFile virtualFile) {
     this.virtualFile = virtualFile;
   }
 
   public Hierarchy createHierarchy(Project project) {
-    FileDocumentManager instance = FileDocumentManager.getInstance();
-    Document document = instance.getDocument(virtualFile);
+    Document document = ActionUtil.getDocument(virtualFile);
 
     JasmineFinder jasmineFinder = new JasmineFinder(project, document);
     jasmineFinder.findAll();
-    hierarchy = new Hierarchy(document, jasmineFinder.getFindResults(), 0);
+    Hierarchy hierarchy = new Hierarchy(document, jasmineFinder.getFindResults(), 0);
 
     return hierarchy;
   }
