395f024e53e567f7a6e53fe3cbd6803535e7f289
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-03-23 12:27:12 -0400

    add test file

---
+++ testData/doubleDescribe.js
@@ -0,0 +1,18 @@
+describe('desc1', function () {
+    it('it1', function () {
+
+    });
+
+    describe('desc1.1', function () {
+        it('it2', function () {
+
+        });
+    });
+});
+
+describe('desc2', function () {
+    it('it3', function () {
+
+    });
+});
+
