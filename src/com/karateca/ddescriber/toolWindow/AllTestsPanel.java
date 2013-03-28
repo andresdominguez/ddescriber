7238433af2a2a7c347ce98db871bcddfd588ad8e
andresdom <andresdom@google.com>
2013-03-28 15:03:20 -0400

    second test, add root and tree

--- src/com/karateca/ddescriber/toolWindow/AllTestsPanel.java
+++ src/com/karateca/ddescriber/toolWindow/AllTestsPanel.java
@@ -1,5 +1,6 @@
 package com.karateca.ddescriber.toolWindow;
 
+import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.treeStructure.Tree;
 import com.karateca.ddescriber.model.TreeNode;
 
@@ -15,14 +16,31 @@ public class AllTestsPanel extends JPanel {
   Tree tree;
   TreeNode root;
   final JPanel topButtonPanel;
-  final JPanel panelWithCurrentTests;
+  final JBScrollPane panelWithCurrentTests;
   final JPanel leftButtonPanel;
 
   public AllTestsPanel() {
     super(new BorderLayout());
 
     topButtonPanel = new JPanel();
-    panelWithCurrentTests = new JPanel();
+    panelWithCurrentTests = buildPanelWithTests();
     leftButtonPanel = new JPanel();
+
+    add(BorderLayout.NORTH, topButtonPanel);
+    add(BorderLayout.CENTER, panelWithCurrentTests);
+    add(BorderLayout.LINE_START, leftButtonPanel);
+  }
+
+  private JBScrollPane buildPanelWithTests() {
+    root = new TreeNode("All tests");
+    tree = new Tree(root);
+
+    JBScrollPane scrollPane = new JBScrollPane(tree);
+
+    // Expand the root an make it invisible.
+    tree.expandRow(0);
+    tree.setRootVisible(false);
+
+    return scrollPane;
   }
 }
--- test/com/karateca/ddescriber/toolWindow/AllTestsPanelTest.java
+++ test/com/karateca/ddescriber/toolWindow/AllTestsPanelTest.java
@@ -1,6 +1,11 @@
 package com.karateca.ddescriber.toolWindow;
 
-import junit.framework.Assert;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNotNull;
+
+import com.intellij.ui.treeStructure.Tree;
+import com.karateca.ddescriber.model.TreeNode;
 
 import org.junit.Test;
 
@@ -13,8 +18,25 @@ public class AllTestsPanelTest {
   public void shouldCreateInternalPanels() {
     AllTestsPanel panel = new AllTestsPanel();
 
-    Assert.assertNotNull(panel.topButtonPanel);
-    Assert.assertNotNull(panel.panelWithCurrentTests);
-    Assert.assertNotNull(panel.leftButtonPanel);
+    // Ensure there are 3 panels inside.
+    assertNotNull(panel.topButtonPanel);
+    assertNotNull(panel.panelWithCurrentTests);
+    assertNotNull(panel.leftButtonPanel);
+
+    assertEquals(3, panel.getComponentCount());
+  }
+
+  @Test
+  public void shouldCreateRootNodeAndTree() {
+    AllTestsPanel panel = new AllTestsPanel();
+
+    TreeNode root = panel.root;
+    Tree tree = panel.tree;
+
+    // Ensure the root is defined.
+    assertEquals("All tests", root.getUserObject());
+
+    // Ensure the root is invisible.
+    assertFalse(tree.isRootVisible());
   }
 }
