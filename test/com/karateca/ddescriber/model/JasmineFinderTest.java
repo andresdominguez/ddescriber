e4ff66230ccaf0ca3c1cb3fdceb45898dd4639da
andresdom <andresdom@google.com>
2015-04-01 15:23:58 -0400

    Add scenarios for jasmine 1, 2

--- test/com/karateca/ddescriber/model/JasmineFinderTest.java
+++ test/com/karateca/ddescriber/model/JasmineFinderTest.java
@@ -5,27 +5,40 @@ import com.karateca.ddescriber.BaseTestCase;
 
 import java.util.List;
 
-/**
- * @author Andres Dominguez.
- */
 public class JasmineFinderTest extends BaseTestCase {
 
   public void testFindAll() throws Exception {
-    List<FindResult> findResults = whenYouFindTestsForJsFile("jasmineTestBefore.js");
+    findsAllTests("jasmine1/jasmineTestBefore.js");
+    findsAllTests("jasmine2/jasmineTestBefore.js");
+  }
+
+  private void findsAllTests(String fileName) {
+    List<FindResult> findResults = whenYouFindTestsForJsFile(fileName);
 
     // Then ensure there are 9 tests and suites.
     assertEquals(9, findResults.size());
   }
 
   public void testWithCaretAtTheTop() {
-    List<FindResult> findResults = whenYouFindTestsForJsFile("jasmineTestCaretTop.js");
+    findsTestsWithCaretAtTheTop("jasmine1/jasmineTestCaretTop.js");
+    findsTestsWithCaretAtTheTop("jasmine2/jasmineTestCaretTop.js");
+  }
+
+  private void findsTestsWithCaretAtTheTop(String fileName) {
+    List<FindResult> findResults = whenYouFindTestsForJsFile(fileName);
 
     // Then ensure there are 6 tests and suites.
     assertEquals(7, findResults.size());
   }
 
   public void testShouldFindTestsWithTrickyText() {
-    List<FindResult> findResults = whenYouFindTestsForJsFile("jasmineWithWeirdRegularExpressions.js");
+    findsTestsWithTrickyText("jasmine1/jasmineWithWeirdRegularExpressions.js");
+    findsTestsWithTrickyText("jasmine2/jasmineWithWeirdRegularExpressions.js");
+  }
+
+  private void findsTestsWithTrickyText(String fileName) {
+    List<FindResult> findResults = whenYouFindTestsForJsFile(
+        fileName);
 
     // Then ensure all the tests were found.
     assertEquals(11, findResults.size());
