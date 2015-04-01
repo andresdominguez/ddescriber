8715cb5b948e5bb6dba5c51109c4d1c9a374b279
andresdom <andresdom@google.com>
2015-04-01 15:26:14 -0400

    Add scenarios for jasmine 1, 2

---
+++ testData/jasmine1/jasmineTestAfter.js
@@ -0,0 +1,51 @@
+/**
+ * Some comments.
+ */
+
+describe('top describe', function () {
+    var a, b, c;
+
+    function someFn(foo, bar) {
+
+    }
+
+    var otherFn = function (hey) {
+    };
+
+    it('first it', function () {
+        console.log('WWWW');
+    });
+
+    it('second it', function () {
+        console.log('WWWW');
+    });
+
+    describe('inner describe', function () {
+        it('inner it 1', function () {
+            var a;
+
+            exit(-1);
+            fixit('');
+            split('');
+
+        });
+
+        it('inner it 2', function () {
+            // <caret>
+            var a;
+        });
+
+        it('inner it 3', function () {
+            var a;
+        });
+
+        describe('the xdescribe', function () {
+
+        });
+
+
+        it('the xit', function () {
+
+        });
+    });
+});
